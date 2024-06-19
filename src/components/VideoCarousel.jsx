264116a4f50fcd5c97561093e7157d82e4b7f9cf
254binaryninja <arnoldmusandu@gmail.com>
2024-06-19 17:00:45 +0300

    video P_2

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,5 +1,6 @@
 import { hightlightsSlides } from "../constants"
-import { useRef , useState} from "react"
+import { useRef , useState,useEffect} from "react"
+import gsap from "gsap";
 
 const VideoCarousel = () => {
     const videoRef = useRef([]);
@@ -14,6 +15,41 @@ const VideoCarousel = () => {
         isPlaying:false,
     })
 
+     const [loadedData,setLoadedData] = useState([])
+
+       const {isEnd, startPlay, videoId, isLastVideo, isPlaying} = video;
+
+       useEffect(()=>{
+        
+         if(loadedData.length > 3 ){
+            if(!isPlaying){
+             videoRef.current[videoId].pause();
+            }
+         }else{
+            startPlay && videoRef.current[videoId].play();
+         }
+
+       },[startPlay, videoId,isPlaying, loadedData])
+
+       useEffect(()=>{
+ 
+         const currentProgress = 0;
+         let span = videoSpanRef;
+
+         if (span[videoId]){
+            // animate progress of the video
+              let anim = gsap.to(span[videoId],{
+                onUpdateParams:()=>{
+
+                },
+                onComplete:()=>{
+
+                }
+              })
+
+         }
+       },[videoId, startPlay])
+
   return (
     <>
     <div className="flex items-center">
@@ -21,7 +57,14 @@ const VideoCarousel = () => {
             <div key={list.id} id="slider" className="sm:pr-20 pr-10">
               <div className="video-carousel_container">
                  <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                    <video id="video" playsInline={true} preload="auto" muted >
+                    <video id="video" playsInline={true} preload="auto" muted 
+                      ref={(el)=>(videoRef.current[i]=el)}
+                      onPlay={()=>{
+                        setVideo((prevVideo)=>({
+                            ...prevVideo, isPlaying:true
+                        }))
+                      }}
+                    >
                         <source src={list.video} type="video/mp4"/>
                     </video>
                  </div>
@@ -33,6 +76,9 @@ const VideoCarousel = () => {
               </div>
             </div>
         ))}
+    </div>
+    <div className="relative flex-center mt-10">
+      <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full"></div>
     </div>
     </>
   )
