006d856f7419b0d7f33ade2e79a72b50edae3541
254binaryninja <arnoldmusandu@gmail.com>
2024-06-19 11:22:25 +0300

    project structuring & nav bar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/Navbar'
 
-function App() {
-  
-
-  return (
-    <main>
-
+const App = () => {
+   return (
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
     </main>
   )
 }
